2d79232a8154de224d26f505452788668050daa9
Piffii <v.poletavkina@gmail.com>
2023-04-15 15:04:31 +0300

    Проверка

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Привет, Яндекс!"
+@app.route('/start')
+def atsrt():
+    return render_template("start.html")
+
+
+@app.route('/registration')
+def registration():
+    return render_template("registration.html")
+
+
+@app.route('/entrance')
+def entrance():
+    return render_template("entrance.html")
+
+
+@app.route('/cosmoexample')
+def cosmoexample():
+    return render_template("cosmoexample.html")
 
 
 if __name__ == '__main__':
