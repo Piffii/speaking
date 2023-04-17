57d553042b013f990b090ea5865d2847122e429a
Piffii <v.poletavkina@gmail.com>
2023-04-17 19:28:00 +0300

    Набросок

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template
+from data import db_session
+from data.users import User
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
-@app.route('/start')
-def atsrt():
+@app.route('/start')  # фотографии с темами в два столбика
+def start():
     return render_template("start.html")
 
-
-@app.route('/registration')
-def registration():
-    return render_template("registration.html")
-
-
-@app.route('/entrance')
-def entrance():
-    return render_template("entrance.html")
-
-
-@app.route('/cosmoexample')
-def cosmoexample():
-    return render_template("cosmoexample.html")
+'''def main():
+    db_session.global_init("db/blogs.db")
+    app.run()'''
 
 
 if __name__ == '__main__':
+    '''main()'''
     app.run(port=8080, host='127.0.0.1')
